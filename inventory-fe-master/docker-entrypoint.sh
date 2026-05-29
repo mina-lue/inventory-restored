58bb99710fc430f85ec2fddce994cb8c2f85ce32
minalu-at-garri <minalu@garrilogistics.com>
2026-05-29 21:52:16 +0300

    Refactor backend URL handling in Docker entrypoint and update Nginx configuration for improved service resolution and static file serving

--- inventory-fe-master/docker-entrypoint.sh
+++ inventory-fe-master/docker-entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 set -e
 
-export PORT="${PORT:-80}"
-export BACKEND_URL="${BACKEND_URL:-http://backend:8080}"
+# 1. Manually substitute $BACKEND_URL from Render into the final Nginx config
+echo "Substituting environment variables..."
+envsubst '$BACKEND_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-case "$BACKEND_URL" in
-  http://*|https://*) ;;
-  *) BACKEND_URL="http://${BACKEND_URL}" ;;
-esac
-
-envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
+# 2. Execute the CMD (which runs Nginx)
 exec "$@"
